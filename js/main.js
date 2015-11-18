6f5122dc47c58d69ead325d01c2274b2cbef19a4
wakahuula <wakahuula@users.noreply.github.com>
2015-11-18 17:20:06 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,42 @@
-(function() {
+function getConfigData() {
+    var backgroundColorPicker = document.getElementById('backgroundColorPicker');
+ 
+    var options = {
+      'backgroundColor': backgroundColorPicker.value
+    };
+    // Save for next launch
+    localStorage['backgroundColor'] = options['backgroundColor'];
+    console.log('Got options: ' + JSON.stringify(options));
+    return options;
+  }
+  function getQueryParam(variable, defaultValue) {
+    var query = location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1]);
+      }
+    }
+    return defaultValue || false;
+  }
+ 
+  var submitButton = document.getElementById('submitButton');
+  submitButton.addEventListener('click', function() {
+    console.log('Submit');
+    // Set the return URL depending on the runtime environment
+    var return_to = getQueryParam('return_to', 'pebblejs://close#');
+    document.location = return_to + encodeURIComponent(JSON.stringify(getConfigData()));
+  });
+  (function() {
+    var backgroundColorPicker = document.getElementById('backgroundColorPicker');
+    // Load any previously saved configuration, if available
+    if(localStorage['backgroundColor']) {
+      backgroundColorPicker.value = localStorage['backgroundColor'];
+    }
+  })();
+
+/*(function() {
   backgroundColorPicker = $('#backgroundColorPicker');
   submitButton = $('#submitButton');
   loadOptions();
@@ -43,4 +81,4 @@ function getQueryParam(variable, defaultValue) {
     }
   }
   return defaultValue || false;
-}
+}*/
