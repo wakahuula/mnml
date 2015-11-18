2dd384193c9e7c105a2c7f4bae770921c9deaf7a
wakahuula <wakahuula@users.noreply.github.com>
2015-11-18 17:00:50 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,25 +16,20 @@ function submitHandler() {
 
 function loadOptions() {
   var $backgroundColorPicker = $('#backgroundColorPicker');
-  var $timeFormatCheckbox = $('#timeFormatCheckbox');
 
   if (localStorage.backgroundColor) {
     $backgroundColorPicker[0].value = localStorage.backgroundColor;
-    $timeFormatCheckbox[0].checked = localStorage.twentyFourHourFormat === 'true';
   }
 }
 
 function getAndStoreConfigData() {
   var $backgroundColorPicker = $('#backgroundColorPicker');
-  var $timeFormatCheckbox = $('#timeFormatCheckbox');
 
   var options = {
     backgroundColor: $backgroundColorPicker.val(),
-    twentyFourHourFormat: $timeFormatCheckbox[0].checked
   };
 
   localStorage.backgroundColor = options.backgroundColor;
-  localStorage.twentyFourHourFormat = options.twentyFourHourFormat;
 
   console.log('Got options: ' + JSON.stringify(options));
   return options;
